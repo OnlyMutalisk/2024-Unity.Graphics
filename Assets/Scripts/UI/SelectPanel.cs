62d2f5539a8d81768b740863427ae129e2843d12
Park No Min <onlymutalisk@gmail.com>
2024-05-25 18:33:12 +0900

    Fix: Repair Upgrade
    1. 중복된 업그레이드가 나오지 않게 수정했습니다.

--- Assets/Scripts/UI/SelectPanel.cs
+++ Assets/Scripts/UI/SelectPanel.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.Reflection;
 using TMPro;
+using UnityEditor.U2D;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -20,6 +21,10 @@ public class SelectPanel : MonoBehaviour
     public SG sg;
     public Character character;
 
+    // 클래스 변수 입니다.
+    private static List<int> rndList = new List<int>();
+    private static int panelCount;
+
     /// <summary>
     /// <br>Resources/Images/Upgrade 에서 랜덤으로 스프라이트를 선택한 뒤</br>
     /// <br>그에 따라 업그레이드 함수와 UI 를 선택합니다.</br>
@@ -29,7 +34,16 @@ public class SelectPanel : MonoBehaviour
     {
         Time.timeScale = 0;
         Sprite[] sprites = Resources.LoadAll<Sprite>("Images/Upgrade");
-        Sprite sprite = sprites[Random.Range(0, sprites.Length)];
+
+        // 업그레이드가 서로 겹치지 않게 보장합니다.
+        int rnd = Random.Range(0, sprites.Length);
+        while (rndList.Contains(rnd) == true)
+        {
+            if (sprites.Length < panelCount) { break; }
+            rnd = Random.Range(0, sprites.Length);
+        }
+        rndList.Add(rnd);
+        Sprite sprite = sprites[rnd];
 
         // 리플렉션으로 게임 매니저에서 스프라이트 이름에 해당하는 텍스트를 가져옵니다.
         image.sprite = sprite;
@@ -69,7 +83,7 @@ public class SelectPanel : MonoBehaviour
         sg.damage *= GameManager.Upgrade_SG_Damage_multiplier;
         Time.timeScale = 1;
     }
-    
+
     /// <summary>
     /// AR 의 탄창 수가 N 발 많아집니다.
     /// </summary>
@@ -95,5 +109,15 @@ public class SelectPanel : MonoBehaviour
     {
         sg.cartridge_max += GameManager.Upgrade_SG_Cartridge_add;
         Time.timeScale = 1;
+    }
+
+    public void Awake()
+    {
+        panelCount++;
+    }
+
+    private void OnDisable()
+    {
+        rndList.Clear();
     }
 }
